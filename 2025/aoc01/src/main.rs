c033e7479668912702bfb3093c99a565073df496
Joey Kurkjian <kurkjianj@gmail.com>
2026-03-04 17:56:19 -0600

    day1 p2

--- 2025/aoc01/src/main.rs
+++ 2025/aoc01/src/main.rs
@@ -7,24 +7,41 @@ fn main() {
     let lines = read_lines("input/input.txt");
 
     let mut pos = 50;
-    let mut counter = 0;
+    let mut finished_zero = 0;
+    let mut hit_zero = 0;
     for line in lines.flatten() {
         let mut chars = line.chars();
         let dir = chars.next().expect("Missing direction");
         let rot = chars.as_str().parse::<i32>().expect("Invalid number");
 
+        let spins = rot / 100;
+        let rot = rot % 100;
+
         match dir {
-            'L' => pos = (pos - rot).rem_euclid(100),
-            'R' => pos = (pos + rot).rem_euclid(100),
+            'L' => {
+                if pos - rot < 0 && pos > 0 {
+                    hit_zero += 1;
+                }
+                pos = (pos - rot).rem_euclid(100);
+            }
+            'R' => {
+                if pos + rot > 100 {
+                    hit_zero += 1;
+                }
+                pos = (pos + rot).rem_euclid(100);
+            }
             _ => panic!("Invalid direction"),
         }
 
         if pos == 0 {
-            counter += 1;
+            finished_zero += 1;
         }
+
+        hit_zero += spins;
     }
 
-    println!("Final count @ 0: {}", counter);
+    println!("Final count @ 0: {}", finished_zero);
+    println!("Clicked zero: {}", hit_zero + finished_zero);
 }
 
 fn read_lines(filename: &str) -> io::Lines<io::BufReader<File>> {
