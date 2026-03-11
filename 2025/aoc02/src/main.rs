ef92da16ae7df4442811e25c2901efca55ed4cd4
Joey Kurkjian <kurkjianj@gmail.com>
2026-03-11 17:12:54 -0500

    day2 p2

--- 2025/aoc02/src/main.rs
+++ 2025/aoc02/src/main.rs
@@ -14,15 +14,30 @@ fn main() {
         let start = start.parse::<usize>().expect("Invalid start");
         let end = end.parse::<usize>().expect("Invalid end");
 
-        for i in start..=end {
+        'outer: for i in start..=end {
             let as_str = i.to_string();
-            if as_str.len() % 2 == 1 {
-                continue;
-            }
-            let (left, right) = as_str.split_at(as_str.len() / 2);
-            if left == right {
+
+            'length: for j in 1..as_str.len() {
+                if as_str.len() % j != 0 {
+                    continue 'length;
+                }
+
+                let substr = as_str[0..j].to_string();
+                for k in (j..as_str.len()).step_by(j) {
+                    // println!("DEBUG: len: {j}, k: {k}");
+                    let other_substr = as_str[k..k + j].to_string();
+                    if substr != other_substr {
+                        continue 'length;
+                    }
+                }
+
                 invalid_sum += i;
+                continue 'outer;
             }
+            // let (left, right) = as_str.split_at(as_str.len() / 2);
+            // if left == right {
+            //     invalid_sum += i;
+            // }
         }
     }
 
