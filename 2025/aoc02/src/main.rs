0ca3163bd42fa3f0efbaade1b378b8960a6c8f8d
Joey Kurkjian <kurkjianj@gmail.com>
2026-03-11 17:31:25 -0500

    [2025-D02] Keep p1 calculation and output

--- 2025/aoc02/src/main.rs
+++ 2025/aoc02/src/main.rs
@@ -8,7 +8,8 @@ fn main() {
     let line = lines.flatten().next().unwrap();
     let ranges = line.split(',').collect::<Vec<_>>();
 
-    let mut invalid_sum = 0;
+    let mut p1 = 0;
+    let mut p2 = 0;
     for range in ranges.into_iter() {
         let (start, end) = range.split_once('-').expect("Invalid range");
         let start = start.parse::<usize>().expect("Invalid start");
@@ -17,6 +18,11 @@ fn main() {
         'outer: for i in start..=end {
             let as_str = i.to_string();
 
+            let (left, right) = as_str.split_at(as_str.len() / 2);
+            if left == right {
+                p1 += i;
+            }
+
             'length: for j in 1..as_str.len() {
                 if as_str.len() % j != 0 {
                     continue 'length;
@@ -24,24 +30,20 @@ fn main() {
 
                 let substr = as_str[0..j].to_string();
                 for k in (j..as_str.len()).step_by(j) {
-                    // println!("DEBUG: len: {j}, k: {k}");
                     let other_substr = as_str[k..k + j].to_string();
                     if substr != other_substr {
                         continue 'length;
                     }
                 }
 
-                invalid_sum += i;
+                p2 += i;
                 continue 'outer;
             }
-            // let (left, right) = as_str.split_at(as_str.len() / 2);
-            // if left == right {
-            //     invalid_sum += i;
-            // }
         }
     }
 
-    println!("Invalid sum: {invalid_sum}");
+    println!("p1 Invalid sum: {p1}");
+    println!("p2 Invalid sum: {p2}");
 }
 
 fn read_lines(filename: &str) -> io::Lines<io::BufReader<File>> {
